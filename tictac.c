aa542551bc0292161c95c17953bab06be2dc8f0d
Wilson Rafael <rafaelmelo75831@gmail.com>
2024-09-09 22:46:31 -0300

    Declared function before main

--- tictac.c
+++ tictac.c
@@ -3,6 +3,15 @@
 #include<ctype.h>
 #include<time.h>
 
+/* Declared functions before main */
+void resetBoard();
+void printBoard();
+void checkFreeSpaces();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner(char);
+
 /* Main in program */
 int main()
 {
@@ -45,6 +54,6 @@ char checkWinner()
 
 void printWinner(char)
 {
-    
+
 }
 
