a4fb374d4bf9b11cb359525bf1e9b257c49800d8
Wilson Rafael <rafaelmelo75831@gmail.com>
2024-09-09 23:40:24 -0300

    check diagonals

--- tictac.c
+++ tictac.c
@@ -127,6 +127,16 @@ char checkWinner()
             return board[0][i];
         }
     }
+    // Check diagonals
+    if(board[0][0] == board[1][1] && board[0][0] == board[2][2])
+    {
+        return board[0][0];
+    }
+    if(board[0][2] == board[1][1] && board[0][2] == board[2][0])
+    {
+        return board[0][2];
+    }
+}
 }
 
 void printWinner(char)
