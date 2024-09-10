514c24f2b3c428053c6ee18b2307b0f35e8a893f
Wilson Rafael <rafaelmelo75831@gmail.com>
2024-09-09 23:05:44 -0300

    include for and if conditions in checkfreespaces function

--- tictac.c
+++ tictac.c
@@ -60,7 +60,18 @@ void printBoard()
 
 int checkFreeSpaces()
 {
+    int freeSpaces = 9;
 
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            if(board[i][j] != ' ')
+            {
+                freeSpaces--;
+            }
+        }
+    }
 }
 
 void playerMove()
